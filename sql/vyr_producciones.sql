0108a129c80200c326b8c121cd4bb31a5232f3d8
marcoquintana-ops <marco.quintanac@gmail.com>
2025-12-08 19:30:42 -0500

    actualizado

--- sql/vyr_producciones.sql
+++ sql/vyr_producciones.sql
@@ -1,22 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2025 a las 05:16:19
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de datos: `vyr_producciones`
 --
@@ -256,6 +237,21 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_login_familia` (IN `p_email` VAR
     LIMIT 1;
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_obtener_evento` (IN `p_id` INT)   BEGIN
+    SELECT 
+        id,
+        nombre,
+        fecha_inicio,
+        fecha_fin,
+        lugar,
+        estado,
+        descripcion,
+        fecha_creacion,
+        fecha_actualizacion
+    FROM eventos
+    WHERE id = p_id;
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_obtener_familia` (IN `p_id` INT)   BEGIN
     SELECT
         id, tipo_documento, numero_documento,
@@ -391,7 +387,7 @@ CREATE TABLE `familias` (
 --
 
 INSERT INTO `familias` (`id`, `tipo_documento`, `numero_documento`, `nombres`, `apellidos`, `email`, `password`, `evento_id`, `nombre_familiar`, `apellidos_Familiar`, `estado`, `fecha_creacion`, `fecha_actualizacion`, `intentos`) VALUES
-(1, 'dni', '09940846', 'Marco Antonio', 'Quintana Camarena', 'marcoquintan@hotmail.com', '$2y$10$7Qd/DDANF1oK2QfgwAEjnex6pLx59pOtqvFma3omDQY4Eb668Tqyq', 1, 'Carlo Marco', 'Quintana Saavedra', 'activo', '2025-11-15 19:46:13', '2025-11-15 21:05:03', 0);
+(1, 'dni', '09940846', 'Marco Antonio', 'Quintana Camarena', 'marcoquintan@hotmail.com', '$2y$10$7Qd/DDANF1oK2QfgwAEjnex6pLx59pOtqvFma3omDQY4Eb668Tqyq', 1, 'Carlo Marco', 'Quintana Saavedra', 'activo', '2025-11-15 19:46:13', '2025-12-05 03:58:09', 0);
 
 -- --------------------------------------------------------
 
